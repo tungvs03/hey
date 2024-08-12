b8e08cf0cdb67167825bcd45e873621087bb5eae
tungvs03 <tungvsfpt@gmail.com>
2024-08-12 14:25:58 +0700

    Remove dist and .vscode from Git tracking

--- hey-backend/dist/respone/http-exception.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HttpException {
-    constructor(status, message) {
-        this.message = message;
-        this.status = status;
-    }
-}
-exports.default = HttpException;
